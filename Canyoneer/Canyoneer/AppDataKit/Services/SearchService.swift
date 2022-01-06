4382d1730d26b5c0ddce90951eccfa15bc5128b5
Brice Pollock <brice.pollock@gmail.com>
2022-01-06 14:46:50 -0800

    implement a basic search algorithm

--- Canyoneer/Canyoneer/AppDataKit/Services/SearchService.swift
+++ Canyoneer/Canyoneer/AppDataKit/Services/SearchService.swift
@@ -8,13 +8,38 @@
 import Foundation
 
 struct SearchService {
+    private let ropeWikiService = RopeWikiService()
     func requestSearch(for searchString: String) -> SearchResultList {
-        return SearchResultList(
-            searchString: searchString,
-            result: [
-                SearchResult(name: "Utah", type: .region, canyonDetails: nil, regionDetails: RopeWikiService.utah),
-                SearchResult(name: "Moonflower", type: .canyon, canyonDetails: RopeWikiService.moonflower, regionDetails: nil)
-            ]
-        )
+        let regions = ropeWikiService.regions()
+        let canyons = ropeWikiService.canyons()
+        let flattenedRegions = flattenRegions(regions: regions)
+        
+        var results = [SearchResult]()
+        canyons.filter { canyon in
+            return canyon.name.lowercased().contains(searchString.lowercased())
+        }.forEach { canyon in
+            results.append(SearchResult(name: canyon.name, type: .canyon, canyonDetails: canyon, regionDetails: nil))
+        }
+        
+        flattenedRegions.filter { region in
+            return region.name.lowercased().contains(searchString.lowercased())
+        }.forEach { region in
+            results.append(SearchResult(name: region.name, type: .region, canyonDetails: nil, regionDetails: region))
+        }
+        return SearchResultList(searchString: searchString, result: results)
+    }
+    
+    func flattenRegions(regions: [Region]) -> [Region] {
+        return regions.flatMap { region in
+            return self.flattenRegion(region: region)
+        }
+    }
+    func flattenRegion(region: Region) -> [Region] {
+        guard !region.children.isEmpty else {
+            return [region]
+        }
+        return region.children.flatMap { region in
+            return self.flattenRegion(region: region)
+        }
     }
 }
